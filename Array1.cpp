e8af0060de97fd5ec35c36ad32c927ff90c92e55
devgeek <nehasinghpro2700@gmail.com>
2024-02-06 22:32:34 +0530

    Day 16 check array sorted or not

--- Array1.cpp
+++ Array1.cpp
@@ -30,6 +30,29 @@ vector<int> getSecondOrderElements(int n, vector<int> a)
     return answer;
 }
 
+//  Check Sorted Array
+int isSorted(int n, vector<int> a)
+{
+    for (int i = 1; i < n; i++)
+    {
+        if (a[i] < a[i - 1])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+//  Remove Duplicates from Sorted Array
+// int removeDuplicates(vector<int> &arr, int n)
+// {
+//     for(int i = 0; i < n; i++){
+//         for(int j = 1; j < n; j++){
+//             if(arr[i])
+//         }
+//     }
+// }
+
 int main()
 {
     // int n;
@@ -52,7 +75,7 @@ int main()
 
     // cout << "Largest Element in an Array: " << LargestElementArray(arr, n) << endl;
 
-    vector<int> varr = {22, 55,66, 12,10};
+    vector<int> varr = {4, 5, 4, 4, 4};
 
     cout << "Vectors Values: ";
     for (auto it : varr)
@@ -60,10 +83,15 @@ int main()
         cout << it << " ";
     }
 
-    auto result = getSecondOrderElements(5, varr);
+    // auto result = getSecondOrderElements(5, varr);
+    auto answer = isSorted(5, varr);
+    // auto result = removeDuplicates(varr, 5);
 
     // Accessing the result vector of pairs
-    cout << "\nSecond smallest and second largest: " << result[0] << " and " << result[1] << endl;
+    // cout << "\nSecond smallest and second largest: " << result[0] << " and " << result[1] << endl;
+    cout << endl;
+    cout << " Check Sorted Array: " << (answer ? "Yes" : "No") << endl;
+    // cout << "Remove Duplicates from Sorted Array: " <<result<< endl;
 
     return 0;
 }
