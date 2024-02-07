074448a83f4e6bd5081f7d051f9d6e1f78c73f30
devgeek <nehasinghpro2700@gmail.com>
2024-02-07 22:17:47 +0530

    Day 17 search , shift and zero's at end

--- Array1.cpp
+++ Array1.cpp
@@ -43,15 +43,138 @@ int isSorted(int n, vector<int> a)
     return true;
 }
 
-//  Remove Duplicates from Sorted Array
+//  Remove Duplicates from Sorted Array with extra space
 // int removeDuplicates(vector<int> &arr, int n)
 // {
+//     vector<int> temp;
 //     for(int i = 0; i < n; i++){
 //         for(int j = 1; j < n; j++){
-//             if(arr[i])
+//             if(arr[i] == arr[j] && i != j){
+//                 arr.erase(arr.begin() +1);
+//                 j--;
+//                 n--;
+//             }
 //         }
+//         temp.push_back(arr[i]);
 //     }
+//     cout<<endl;
+//     cout << "arr array: ";
+//     for (auto it : temp)
+//     {
+//         cout << it << " ";
+//     }
+
+//     return temp.size();
 // }
+
+//  Remove Duplicates from Sorted Array with extra space
+int removeDuplicates(vector<int> &arr, int n)
+{
+    int tempindex = 1;
+    if (n == 0)
+    {
+        return 0;
+    }
+
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] != arr[i - 1])
+        {
+            arr[tempindex++] = arr[i];
+        }
+    }
+
+    arr.resize(tempindex);
+
+    return tempindex;
+}
+
+//  Left Rotate an Array by One
+
+vector<int> rotateArrayleft(vector<int> &arr, int n)
+{
+    if (n == 0 || n == 1)
+        return arr;
+
+    int firstVal = arr[0];
+
+    for (int i = 1; i < n; i++)
+    {
+        arr[i - 1] = arr[i];
+    }
+
+    arr[n - 1] = firstVal;
+
+    return arr;
+}
+
+//  Rotate array by k times
+
+vector<int> rotateArraybyktimes(vector<int> arr, int k)
+{
+    int n = arr.size();
+    if (n == 0 || n == 1 || k == 0)
+        return arr;
+
+    for (int i = 1; i <= k; i++)
+    {
+        int firstvalue = arr[0];
+        for (int j = 1; j < n; j++)
+        {
+            arr[j - 1] = arr[j];
+        }
+        arr[n - 1] = firstvalue;
+
+        cout << endl;
+        cout << "arr array " << i << " times: ";
+        for (auto it : arr)
+        {
+            cout << it << " ";
+        }
+    }
+
+    return arr;
+}
+
+//  Move Zero's to End
+
+vector<int> moveZeros(int n, vector<int> a)
+{
+    int tempidx = 0;
+    if (n == 0 || n == 1)
+        return a;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] != 0)
+        {
+            a[tempidx++] = a[i];
+        }
+    }
+
+    a.resize(tempidx);
+
+    for (int i = tempidx; i < n; i++)
+    {
+        a.push_back(0);
+    }
+
+    return a;
+}
+
+//  Linear Search
+
+int linearSearch(int n, int num, vector<int> &arr)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == num)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
 
 int main()
 {
@@ -75,7 +198,7 @@ int main()
 
     // cout << "Largest Element in an Array: " << LargestElementArray(arr, n) << endl;
 
-    vector<int> varr = {4, 5, 4, 4, 4};
+    vector<int> varr = {2, 5, 6, 2};
 
     cout << "Vectors Values: ";
     for (auto it : varr)
@@ -84,14 +207,33 @@ int main()
     }
 
     // auto result = getSecondOrderElements(5, varr);
-    auto answer = isSorted(5, varr);
-    // auto result = removeDuplicates(varr, 5);
+    // auto answer = isSorted(5, varr);
+    // auto result = removeDuplicates(varr, 9);
+    // auto result = rotateArrayleft(varr, 5);
+    // int k = 3;
+    // auto result = rotateArraybyktimes(varr, k);
+    // auto result = moveZeros(5, varr);
+    int num = 20;
+    auto result = linearSearch(4, num, varr);
 
     // Accessing the result vector of pairs
     // cout << "\nSecond smallest and second largest: " << result[0] << " and " << result[1] << endl;
     cout << endl;
-    cout << " Check Sorted Array: " << (answer ? "Yes" : "No") << endl;
+    // cout << " Check Sorted Array: " << (answer ? "Yes" : "No") << endl;
     // cout << "Remove Duplicates from Sorted Array: " <<result<< endl;
+    // cout << "Rotate array K Times: ";
 
+    // for (auto it : result)
+    // {
+    //     cout << it << " ";
+    // }
+
+    // cout << "Move Zero's to End: ";
+    // for (auto it : result)
+    // {
+    //     cout << it << " ";
+    // }
+
+    cout << "Linear Search: " << result << endl;
     return 0;
 }
