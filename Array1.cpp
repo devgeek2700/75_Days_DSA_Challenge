304d92ceb6519c8f48ef0ba9d525e1a298695636
devgeek <nehasinghpro2700@gmail.com>
2024-02-18 09:12:07 +0530

    Day 28 Spiral matrix

--- Array1.cpp
+++ Array1.cpp
@@ -911,7 +911,53 @@ void rotateMatrix(vector<vector<int>> &mat)
     }
 }
 
+//  Spiral Matrix --> only one optimal way to solve this problem
 
+vector<int> spiralMatrix(vector<vector<int>> &MATRIX)
+{
+    int n = MATRIX.size();
+    int m = MATRIX[0].size();
+
+    int left = 0, right = m - 1;
+    int top = 0, bottom = n - 1;
+    vector<int> ans;
+
+    while (top <= bottom && left <= right)
+    {
+
+        for (int i = left; i <= right; i++)
+        {
+            ans.push_back(MATRIX[top][i]);
+        }
+        top++;
+
+        for (int i = top; i <= bottom; i++)
+        {
+            ans.push_back(MATRIX[i][right]);
+        }
+        right--;
+
+        if (top <= bottom)
+        {
+            for (int i = right; i >= left; i--)
+            {
+                ans.push_back(MATRIX[bottom][i]);
+            }
+            bottom--;
+        }
+
+        if (left <= right)
+        {
+            for (int i = bottom; i >= top; i--)
+            {
+                ans.push_back(MATRIX[i][left]);
+            }
+            left++;
+        }
+    }
+
+    return ans;
+}
 
 int main()
 {
@@ -943,7 +989,7 @@ int main()
         {19, 20, 21, 50},
         {59, 60, 71, 80}
     };
-
+    
     // vector<int> barr = {2 ,3 ,4 ,6, 7};
 
     cout << "Vector Values: " << endl;
