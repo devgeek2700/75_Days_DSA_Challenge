00a7b7ef276969befa59d139f7dbe8049073406f
devgeek <nehasinghpro2700@gmail.com>
2024-02-23 15:21:12 +0530

    four sum

--- Array1.cpp
+++ Array1.cpp
@@ -1113,6 +1113,44 @@ vector<vector<int>> triplet(int n, vector<int> &arr)
     return result;
 }
 
+// four sum
+
+vector<vector<int>> fourSum(vector<int>& nums, int target) {
+    vector<vector<int>> result;
+    int n = nums.size();
+    sort(nums.begin(), nums.end());
+
+    for (int i = 0; i < n - 3; ++i) {
+        // Avoid duplicate quadruplets
+        if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+        for (int j = i + 1; j < n - 2; ++j) {
+            // Avoid duplicate quadruplets
+            if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+
+            int left = j + 1, right = n - 1;
+            int remaining = target - nums[i] - nums[j];
+
+            while (left < right) {
+                int sum = nums[left] + nums[right];
+                if (sum == remaining) {
+                    result.push_back({nums[i], nums[j], nums[left], nums[right]});
+                    // Avoid duplicate quadruplets
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+                    left++;
+                    right--;
+                } else if (sum < remaining) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+    }
+
+    return result;
+}
 
 int main()
 {
