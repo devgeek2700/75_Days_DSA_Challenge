943c0426bce98f80ef94f4712cfb69f7ae0e1487
devgeek <nehasinghpro2700@gmail.com>
2024-03-09 21:05:53 +0530

    Day 47 first , last occurrence and count of occurrence

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -177,7 +177,7 @@ pair<int, int> getFloorAndCeil(vector<int> &arr, int n, int x)
         }
         else
         {
-            ceil = arr[mid]; //4 > 2  --> 3
+            ceil = arr[mid]; // 4 > 2  --> 3
             high = mid - 1;
         }
     }
@@ -185,12 +185,156 @@ pair<int, int> getFloorAndCeil(vector<int> &arr, int n, int x)
     return {floor, ceil};
 }
 
+//  First and Last Position of an Element In Sorted Array
+
+// pair<int, int> firstAndLastPosition(vector<int> &arr, int n, int k) // TC --> O(n)  SC --> O(1)
+// {
+//     int first = -1;
+//     int last = -1;
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         if (arr[i] == k)
+//         {
+//             if (first == -1)
+//             {
+//                 first = i;
+//             }
+//             else
+//             {
+//                 last = i;
+//             }
+//         }
+//         if (last == -1)
+//         {
+//             last = first;
+//         }
+//     }
+
+//     return {first, last};
+// }
+
+// Optimal soln --> using libraies
+// pair<int, int> firstAndLastPosition(vector<int> &arr, int n, int k) {
+//     int first = lower_bound(arr.begin(), arr.end(), k) - arr.begin();
+//     int last = (upper_bound(arr.begin(), arr.end(), k) - arr.begin()) - 1;
+
+//     if (first == n || arr[first] != k) {
+//         first = last = -1;
+//     }
+
+//     return {first, last};
+// }
+
+// Optimal soln --> using first and last function call
+
+int firstPosition(vector<int> &arr, int n, int k)
+{
+    int low = 0;
+    int first = -1;
+    int high = n - 1;
+
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+
+        if (arr[mid] == k)
+        {
+            first = mid;
+            high = mid - 1;
+        }
+        else if (arr[mid] > k)
+        {
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+    return first;
+}
+
+int lastPosition(vector<int> &arr, int n, int k)
+{
+    int low = 0;
+    int last = -1;
+    int high = n - 1;
+
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+
+        if (arr[mid] == k)
+        {
+            last = mid;
+            low = mid + 1;
+        }
+        else if (arr[mid] > k)
+        {
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+    return last;
+}
+
+pair<int, int> firstAndLastPosition(vector<int> &arr, int n, int k)
+{
+    int first = firstPosition(arr, n, k);
+    if (first == -1)
+    {
+        return {-1, -1};
+    }
+    int last = lastPosition(arr, n, k);
+
+    return {first, last};
+}
+
+//  Number of occurrence  --> using libraies
+
+// int countofoccurrence(vector<int> &arr, int n, int x) 
+// {
+//     int first = lower_bound(arr.begin(), arr.end(), x) - arr.begin();
+//     int last = (upper_bound(arr.begin(), arr.end(), x) - arr.begin()) - 1;
+
+//     int count = last - first + 1;
+
+//     return count;
+// }
+
+// Optimal soln --> using first and last function call
+
+int countofoccurrence(vector<int> &arr, int n, int x) 
+{
+    int first = firstPosition(arr, n, x);
+    if (first == -1)
+    {
+        return 0;
+    }
+    int last = lastPosition(arr, n, x);
+
+    return last - first +1;
+}
+
+
+//  Search In Rotated Sorted Array (I) --> all values are unique
+
+int RotatedArrayI(vector<int>& arr, int n, int k)
+{
+    
+}
+
+
 int main()
 {
-    vector<int> arr = {3, 4, 4, 7, 8, 10};
+    vector<int> arr = {1, 1, 1, 2, 2, 3, 3};
 
     int n = arr.size();
-    int target = 2;
+    int target = 3;
 
     cout << "Display Arrays: ";
     for (int i = 0; i < n; i++)
@@ -214,8 +358,17 @@ int main()
     // int result = searchInsert(arr, target);
     // cout << "Search Insert Position: " << result << endl;
 
-    pair<int, int> result = getFloorAndCeil(arr, n, target);
-    cout << "Search Insert Position: " << result.first << " " << result.second << endl;
+    // pair<int, int> result = getFloorAndCeil(arr, n, target);
+    // cout << "Search Insert Position: " << result.first << " " << result.second << endl;
+
+    // pair<int, int> result = firstAndLastPosition(arr, n, target);
+    // cout << "First and Last Position of an Element In Sorted Array: " << result.first << " " << result.second << endl;
+
+    // int result = countofoccurrence(arr, n, target);
+    // cout << "Number of occurrence: " << result << endl;
+
+    int result = RotatedArrayI(arr, n, target);
+    cout << "Search In Rotated Sorted Array (I): " << result << endl;
 
     return 0;
 }
