5e4ac29caff338916c81fdf3d02aa5d2e5b239a2
devgeek <nehasinghpro2700@gmail.com>
2024-03-19 21:48:46 +0530

    Day 57 leastWeightCapacity, smallestDivisor

--- BinarySearch2.cpp
+++ BinarySearch2.cpp
@@ -137,7 +137,6 @@ bool possibleBloomDay(vector<int> arr, int k, int m, int day)
         }
     }
 
-
     noofBouquets += (count / k);
 
     if (noofBouquets >= m)
@@ -152,9 +151,8 @@ bool possibleBloomDay(vector<int> arr, int k, int m, int day)
 
 int roseGarden(vector<int> arr, int k, int m)
 {
-    //m -->  no of Bouquets
-    //k -->  adjacent flowers required
-
+    // m -->  no of Bouquets
+    // k -->  adjacent flowers required
 
     int low = *min_element(arr.begin(), arr.end());
     int high = *max_element(arr.begin(), arr.end());
@@ -178,17 +176,110 @@ int roseGarden(vector<int> arr, int k, int m)
     return ans;
 }
 
+//  Smallest divisor
 
+int allpossibleDivisor(vector<int> arr, int mid)
+{
+    int totalDivisor = 0;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        totalDivisor += ceil(double(arr[i]) / mid);
+    }
+    return totalDivisor;
+}
+
+int smallestDivisor(vector<int> &arr, int limit)
+{
+    int low = 1;
+    int high = *max_element(arr.begin(), arr.end());
+    int n = arr.size();
+    int ans = -1;
+
+    if (n > limit)
+    {
+        return ans;
+    }
+
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        int requiredAns = allpossibleDivisor(arr, mid);
+
+        if (requiredAns <= limit)
+        {
+            ans = mid;
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+    return ans;
+}
+
+//  Capacity To Ship Packages Within D Days
+
+int allpossibleDays(vector<int> weights, int Capacity)
+{
+    int noofDays = 1;
+    int load = 0;
+
+    for (int i = 0; i < weights.size(); i++)
+    {
+        if (load + weights[i] > Capacity)
+        {
+            noofDays += 1;
+            load = weights[i];
+        }
+        else
+        {
+            load += weights[i];
+        }
+    }
+    return noofDays;
+}
+
+int leastWeightCapacity(vector<int> &weights, int d)
+{
+    // low has to be maximum of the capacity so that each & every weights is transfered
+    // high has to be maximum of the capacity so that each & every weights is transfered
+
+    int n = weights.size();
+    int low = *max_element(weights.begin(), weights.end());
+    int high = accumulate(weights.begin(), weights.end(), 0);
+    int ans = -1;
+    
+
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        int requiredAns = allpossibleDays(weights, mid);
+
+        if (requiredAns <= d)
+        {
+            ans = mid;
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+    return ans;
+}
 
 int main()
 {
     // int n = 4;
     // int h = 7;
+    // int k = 1;
+    // int m = 1;
+    // int limit = 11;
 
-    vector<int> arr = {1, 1, 1, 1};
+    vector<int> arr = {86 ,45, 74, 64, 14 ,68, 44 ,11, 43, 33, 8, 20};
     int n = arr.size();
-    int k = 1;
-    int m = 1;
+    int d = 12;
 
     cout << "Display Arrays: ";
     for (int i = 0; i < n; i++)
@@ -200,8 +291,14 @@ int main()
     // int result = minimumRateToEatBananas(arr, h);
     // cout << "Koko Eating Bananas: " << result << endl;
 
-    int result = roseGarden(arr, k, m);
-    cout << "Rose Garden: " << result << endl;
+    // int result = roseGarden(arr, k, m);
+    // cout << "Rose Garden: " << result << endl;
+
+    // int result = smallestDivisor(arr, limit);
+    // cout << "Smallest Divisor Given a limit: " << result << endl;
+
+    int result = leastWeightCapacity(arr, d);
+    cout << "Capacity To Ship Packages Within D Days: " << result << endl;
 
     return 0;
 }
