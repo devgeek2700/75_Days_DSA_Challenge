3ecd6e956710b0aedbabaa2af6fcddcec8e004b2
devgeek <nehasinghpro2700@gmail.com>
2024-03-21 22:25:34 +0530

    Day 59 Allocate Books

--- BinarySearchonminmaxversa.cpp
+++ BinarySearchonminmaxversa.cpp
@@ -55,10 +55,64 @@ int aggressiveCows(vector<int> &stalls, int k)
     return ans;
 }
 
+
+
+//  Allocate Books
+
+int maxminumPagesAllocate(vector<int> &arr, int mid)
+{
+    int CurrentStudent = 1;
+    int pagesHold = 0;
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        if (pagesHold + arr[i] <= mid)
+        {
+            pagesHold += arr[i];
+        }
+        else
+        {
+            CurrentStudent += 1;
+            pagesHold = arr[i];
+        }
+    }
+
+    return CurrentStudent;
+}
+
+int findPages(vector<int> &arr, int n, int m)
+{ // --> max on min
+    int low = *max_element(arr.begin(), arr.end());
+    int high = accumulate(arr.begin(), arr.end(), 0);
+    int ans = -1;
+
+    if(m > n){
+        return ans;
+    }
+
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        int noofStudents = maxminumPagesAllocate(arr, mid);
+
+        if (noofStudents > m)
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            ans = mid;
+            high = mid - 1;
+        }
+    }
+    return ans;
+}
+
 int main()
 {
-    vector<int> arr = {4, 2, 1, 3, 6};
-    int k = 2;
+    vector<int> arr = {25, 46, 28, 49, 24};
+    // int k = 2;
+    int m = 4;
     int n = arr.size();
 
     cout << "Display Arrays: ";
@@ -68,8 +122,11 @@ int main()
     }
     cout << endl;
 
-    int result = aggressiveCows(arr, k);
-    cout << "Aggressive Cows: " << result << endl;
+    // int result = aggressiveCows(arr, k);
+    // cout << "Aggressive Cows: " << result << endl;
+
+    int result = findPages(arr, n, m);
+    cout << "Allocate Books: " << result << endl;
 
     return 0;
 }
