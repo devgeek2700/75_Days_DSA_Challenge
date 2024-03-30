4f740fd5c19ac270f3da3c546d97660c6ce1791d
devgeek <nehasinghpro2700@gmail.com>
2024-03-30 22:01:42 +0530

    Day 68 Find A Peak Element in 2D Matrix

--- BinarySearch2D.cpp
+++ BinarySearch2D.cpp
@@ -105,13 +105,52 @@ bool searchElementII(vector<vector<int>> &MATRIX, int target)
     return false;
 }
 
+//  Find A Peak Element in 2D Matrix
+
+int FindMaxNuminCol(vector<vector<int>>& g, int n, int m, int midcol) {
+    int maxValCol = -1;
+    int maxNumidx = -1;
+
+    for (int i = 0; i < n; i++) {
+        if (g[i][midcol] > maxValCol) {
+            maxValCol = g[i][midcol];
+            maxNumidx = i;
+        }
+    }
+    return maxNumidx;
+}
+
+vector<int> findPeakGrid(vector<vector<int>>& g) {
+    int n = g.size();
+    int m = g[0].size();
+    int low = 0;
+    int high = m - 1;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        int maxRowIdx = FindMaxNuminCol(g, n, m, mid);
+        int left = mid - 1 >= 0 ? g[maxRowIdx][mid - 1] : -1;
+        int right = mid + 1 < m ? g[maxRowIdx][mid + 1] : -1;
+
+        if (g[maxRowIdx][mid] > left && g[maxRowIdx][mid] > right) {
+            return {maxRowIdx, mid};
+        }
+        else if (g[maxRowIdx][mid] < left) {
+            high = mid - 1;
+        }
+        else {
+            low = mid + 1;
+        }
+    }
+    return {-1, -1};
+}
+
 int main()
 {
     vector<vector<int>> matrix = {
-        {1, 5, 9, 13},
-        {14, 15, 16, 17},
-        {19, 20, 21, 50},
-        {59, 60, 71, 80}};
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}};
 
     int n = matrix.size();
     int m = matrix[0].size();
@@ -135,8 +174,11 @@ int main()
     // int max1sRow = searchMatrix(matrix, target);
     // cout << "Search In A 2D Matrix: " << max1sRow << endl;
 
-    int max1sRow = searchElementII(matrix, target);
-    cout << "Search In A 2D Matrix - II: " << max1sRow << endl;
+    // int max1sRow = searchElementII(matrix, target);
+    // cout << "Search In A 2D Matrix - II: " << max1sRow << endl;
+
+    vector<int> result = findPeakGrid(matrix);
+    cout << "Find A Peak Element: (" << result[0] << ", " << result[1] << ")" << endl;
 
     return 0;
 }
