66f11d63dc45d247d0d939c6a033e5a159044dec
devgeek <nehasinghpro2700@gmail.com>
2024-05-23 22:06:49 +0530

    Day 119 Longest Substring Without Repeating Characters

--- SlidingWindow_twoPointers.cpp
+++ SlidingWindow_twoPointers.cpp
@@ -1,36 +1,69 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int solve(string str) {
-
-  if(str.size()==0)
-      return 0;
-  int maxans = INT_MIN;
-  for (int i = 0; i < str.length(); i++) // outer loop for traversing the string
-  {
-    unordered_set < int > set;
-    for (int j = i; j < str.length(); j++) // nested loop for getting different string starting with str[i]
+// Longest Substring Without Repeating Characters
+// bructe force solution
+int lengthOfLongestSubstring1(string str) // TC --> O(N^2)  SC --> O(n,256)
+{
+    int maxLen = INT_MIN;
+    int n = str.length();
+    for (int i = 0; i < n; i++)
     {
-      if (set.find(str[j]) != set.end()) // if element if found so mark it as ans and break from the loop
-      {
-        maxans = max(maxans, j - i);
-        break;
-      }
-      set.insert(str[j]);
+        map<int, int> map;
+        for (int j = i; j < n; j++)
+        {
+            if (map.find(str[j]) != map.end())
+            {
+                break;
+            }
+            int currLen = j - i + 1;
+            maxLen = max(maxLen, currLen);
+            map[str[j]] = 1;
+        }
     }
-  }
-  return maxans;
+    return maxLen;
 }
 
-int consecutiveOnes(vector<int>& arr) {
+// Optimal Approach
+// for finding maximum substring --> two pointer and Sliding Windows
+int lengthOfLongestSubstring(string str) // TC --> O(N)  SC --> O(n,256)
+{
+    int maxLen = INT_MIN;
+    int n = str.length();
+    int left = 0;
+    int right = 0;
+    unordered_map<char, int> charIdx;
+
+    while (right < n)
+    {
+        if (charIdx.count(str[right]))
+        {
+            left = max(left, charIdx[str[right]] + 1);
+        }
+
+        charIdx[str[right]] = right;
+        int currLen = right - left + 1;
+        maxLen = max(maxLen, currLen);
+        right++;
+    }
+
+    return maxLen;
+}
+
+int consecutiveOnes(vector<int> &arr)
+{
     int max_length = 0;
     int current_length = 0;
 
-    for (int i = 0; i < arr.size(); ++i) {
-        if (arr[i] == 1) {
+    for (int i = 0; i < arr.size(); ++i)
+    {
+        if (arr[i] == 1)
+        {
             current_length++;
             max_length = max(max_length, current_length);
-        } else {
+        }
+        else
+        {
             current_length = 0;
         }
     }
@@ -40,39 +73,12 @@ int consecutiveOnes(vector<int>& arr) {
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    string str = "abcabcbb";
+
+    cout << "string is: " << str << endl;
+
+    int result = lengthOfLongestSubstring(str);
+    cout << "Longest Substring Without Repeating Characters: " << result << endl;
+
     return 0;
-}
-
-
-
-int countAtMostKDistinct(vector<int>& arr, int n, int k) {
-    if (k == 0) return 0;
-    
-    unordered_map<int, int> freq;
-    int left = 0, right = 0, count = 0, distinctCount = 0;
-
-    while (right < n) {
-        if (freq[arr[right]] == 0) {
-            distinctCount++;
-        }
-        freq[arr[right]]++;
-        right++;
-
-        while (distinctCount > k) {
-            freq[arr[left]]--;
-            if (freq[arr[left]] == 0) {
-                distinctCount--;
-            }
-            left++;
-        }
-
-        count += (right - left);
-    }
-
-    return count;
-}
-
-int goodSubarrays(vector<int>& arr, int n, int b) {
-    return countAtMostKDistinct(arr, n, b) - countAtMostKDistinct(arr, n, b - 1);
 }
