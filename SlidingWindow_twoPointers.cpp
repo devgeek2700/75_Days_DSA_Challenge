570b047cacfa1458850049e5d7c8db5ef97b878e
devgeek <nehasinghpro2700@gmail.com>
2024-05-31 22:45:18 +0530

    Day 127 Extra

--- SlidingWindow_twoPointers.cpp
+++ SlidingWindow_twoPointers.cpp
@@ -586,3 +586,30 @@ int main()
 
     return 0;
 }
+
+vector<int> maximumsumSubarray(vector<int> &arr , int n)
+{
+     int max_sum = arr[0], current_sum = arr[0];
+    int start = 0, end = 0, temp_start = 0;
+    
+    for (int i = 1; i < n; ++i) {
+        if (current_sum + arr[i] < arr[i]) {
+            current_sum = arr[i];
+            temp_start = i;
+        } else {
+            current_sum += arr[i];
+        }
+
+        if (current_sum > max_sum) {
+            max_sum = current_sum;
+            start = temp_start;
+            end = i;
+        } else if (current_sum == max_sum && (i - temp_start > end - start)) {
+            // If sums are equal, choose the longer subarray
+            start = temp_start;
+            end = i;
+        }
+    }
+    
+    return vector<int>(arr.begin() + start, arr.begin() + end + 1);
+}
