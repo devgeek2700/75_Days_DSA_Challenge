a20a663865c3216a8674c098dc21860824d76ebf
devgeek <nehasinghpro2700@gmail.com>
2024-06-16 21:53:57 +0530

    Day 141 Roman to Integer,  String to Integer (atoi)

--- String2.cpp
+++ String2.cpp
@@ -27,7 +27,7 @@ string frequencySort(string s) // TC --> O(n+nlogn)  SC --> O(n+m)
 }
 
 // Maximum Nesting Depth of the Parentheses
-int maxDepth(string s)
+int maxDepth(string s) // TC --> O(N) SC --> O(1)
 {
     int n = s.length();
     int maxDepth = INT_MIN;
@@ -48,16 +48,94 @@ int maxDepth(string s)
     return maxDepth;
 }
 
+// Roman to Integer
+int romanToInt(string str) // TC --> O(N) SC --> O(1)
+{
+    int n = str.length();
+    int total = 0;
+
+    unordered_map<char, int> roman{{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
+
+    for (int i = 0; i < n; i++)
+    {
+        if (i + 1 < n && roman[str[i]] < roman[str[i + 1]])
+        {
+            total -= roman[str[i]];
+        }
+        else
+        {
+            total += roman[str[i]];
+        }
+    }
+    return total;
+}
+
+//  String to Integer (atoi)
+int myAtoi(string str) // TC --> O(N) SC --> O(1)
+{
+    int n = str.length();
+    int i = 0;
+
+    // Check leading whitespace.
+    while (i <= n && str[i] == ' ')
+    {
+        i++;
+    }
+
+    // Check sign character.
+    int sign = 1;
+    if (i < n && (str[i] == '-' || str[i] == '+'))
+    {
+        if (str[i] == '-')
+        {
+            sign = -1;
+        }
+        else
+        {
+            sign = 1;
+        }
+        // sign = (str[i] = '-') ? -1 : +1;
+        i++;
+    }
+
+    long long ans = 0;
+
+    while (i < n && isdigit(str[i]))
+    {
+        ans = ans * 10 + (str[i] - '0');
+
+        // Check for overflow and underflow
+        if (sign * ans <= INT_MIN)
+        {
+            return INT_MIN;
+        }
+
+        if (sign * ans >= INT_MAX)
+        {
+            return INT_MAX;
+        }
+        i++;
+    }
+
+    return sign * ans;
+}
+
 int main()
 {
-    string str = "()(())((()()))";
+    string str = "+9";
     cout << "String: " << str << endl;
 
     // auto result = frequencySort(str);
     // cout << "Sorted String: " << result << endl;
 
-    auto result = maxDepth(str);
-    cout << "Maximum Nesting Depth of the Parentheses: " << result << endl;
+    // auto result = maxDepth(str);
+    // cout << "Maximum Nesting Depth of the Parentheses: " << result << endl;
+
+    // auto result = romanToInt(str);
+    // cout << "Roman to Integer: " << result << endl;
+
+    auto result = myAtoi(str);
+    cout << "String to Integer (atoi): " << result << endl;
 
     return 0;
 }
