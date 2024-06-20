82ecb5bf66bd8f8325a838b5e6d6f5d7b6296ad9
devgeek <nehasinghpro2700@gmail.com>
2024-06-20 20:56:38 +0530

    day 145 String to Integer (atoi)

--- RecursionSum.cpp
+++ RecursionSum.cpp
@@ -32,7 +32,7 @@ double myPow1(double x, int n)
     return ans;
 }
 
-// Recursion
+// Power of n Recursion
 
 double myPow(double x, int n)
 {
@@ -58,14 +58,78 @@ double myPow(double x, int n)
     }
 }
 
+// String to Integer (atoi)
+
+int recursionmyAtoi(string str, int i, int sign, int ans)
+{
+    if (i >= str.length() || !isdigit(str[i]))
+    {
+        return ans;
+    }
+
+    // Get the current digit and update the answer
+    int digit = str[i] - '0';
+
+    // Check for overflow before updating ans
+    if (ans > (INT_MAX - digit) / 10)
+    {
+        return sign == 1 ? INT_MAX : INT_MIN;
+    }
+
+    ans = ans * 10 + digit;
+
+    return recursionmyAtoi(str, i + 1, sign, ans);
+}
+
+int myAtoi(string str)
+{
+    int n = str.length();
+    int i = 0;
+
+    // Check leading whitespace.
+    while (i <= n && str[i] == ' ')
+    {
+        i++;
+    }
+
+    // Check sign character.
+    int sign = 1;
+    if (i < n && (str[i] == '-' || str[i] == '+'))
+    {
+        if (str[i] == '-')
+        {
+            sign = -1;
+        }
+        else
+        {
+            sign = 1;
+        }
+        // sign = (str[i] = '-') ? -1 : +1;
+        i++;
+    }
+
+    if (i < n && isdigit(str[i]))
+    {
+        long long result = recursionmyAtoi(str, i, sign, 0);
+        return sign * result;
+    }
+
+    return 0;
+}
+
 int main()
 {
-    double x = 2.00000;
-    int n = -2;
-    cout << "x: " << x << " and n: " << n << endl;
+    // double x = 2.00000;
+    // int n = -2;
+    string str = "words and 987";
+    // cout << "x: " << x << " and n: " << n << endl;
+    cout << "str: " << str << endl;
 
-    auto result = myPow(x, n);
-    cout << "Pow(x, n): " << result << endl;
+    // auto result = myPow(x, n);
+    // cout << "Pow(x, n): " << result << endl;
+
+    auto result = myAtoi(str);
+    cout << "String to Integer (atoi): " << result << endl;
 
     return 0;
 }
