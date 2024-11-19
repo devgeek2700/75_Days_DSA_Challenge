5e827caff6e470e86f27674615f63a8563c43223
devgeek <nehasinghpro2700@gmail.com>
2024-11-19 22:27:07 +0530

    newDay 28 array medium

--- tcs.cpp
+++ tcs.cpp
@@ -255,6 +255,412 @@ int lenOfLongestSubarr(vector<int> &arr, int k)
     return maxLen;
 }
 
+// ************************* ARRAY MEDIUM ***************************
+
+// Two Sum
+vector<int> twoSum(vector<int> &nums, int target)
+{
+    unordered_map<int, int> mpp;
+    vector<int> ans;
+    int n = nums.size();
+
+    for (int i = 0; i < n; i++)
+    {
+        int comp = target - nums[i];
+
+        if (mpp.find(comp) != mpp.end())
+        {
+            ans.push_back(mpp[comp]);
+            ans.push_back(i);
+            return ans;
+        }
+        mpp[nums[i]] = i;
+    }
+    return ans;
+}
+
+// Sort Colors
+void sortColors1(vector<int> &nums)
+{
+    int n = nums.size();
+    int count0 = 0;
+    int count1 = 0;
+    int count2 = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (nums[i] == 0)
+        {
+            count0++;
+        }
+        else if (nums[i] == 1)
+        {
+            count1++;
+        }
+        else
+        {
+            count2++;
+        }
+    }
+
+    for (int i = 0; i < count0; i++)
+    {
+        nums[i] = 0;
+    }
+
+    for (int i = count0; i < count0 + count1; i++)
+    {
+        nums[i] = 1;
+    }
+    for (int i = count0 + count1; i < n; i++)
+    {
+        nums[i] = 2;
+    }
+}
+
+// Sort Colors --> Mthd-2
+void sortColors(vector<int> &nums)
+{
+    int n = nums.size();
+    int low = 0;
+    int high = n - 1;
+    int mid = 0;
+
+    while (mid <= high)
+    {
+        if (nums[mid] == 0)
+        {
+            swap(nums[low], nums[mid]);
+            low++;
+            mid++;
+        }
+        else if (nums[mid] == 1)
+        {
+            mid++;
+        }
+        else
+        {
+            swap(nums[mid], nums[high]);
+            high--;
+        }
+    }
+}
+
+// Majority Element
+int majorityElement(vector<int> &nums)
+{
+    int n = nums.size();
+    map<int, int> mpp;
+
+    for (int i = 0; i < n; i++)
+    {
+        mpp[nums[i]]++;
+    }
+
+    for (auto it : mpp)
+    {
+        if (it.second > (n / 2))
+        {
+            return it.first;
+        }
+    }
+    return -1;
+}
+
+// Maximum Subarray
+int maxSubArray(vector<int> &nums)
+{
+    int n = nums.size();
+    int maxSum = INT_MIN;
+    int sum = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        sum += nums[i];
+        maxSum = max(maxSum, sum);
+        if (sum < 0)
+        {
+            sum = 0;
+        }
+    }
+    return maxSum;
+}
+
+// Maximum Score from Subarray Minimums
+int pairWithMaxSum(vector<int> &arr)
+{
+    int n = arr.size();
+    int maxScore = INT_MIN;
+    if (n < 2)
+    {
+        return 0;
+    }
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        int currSum = arr[i] + arr[i + 1];
+        maxScore = max(maxScore, currSum);
+    }
+    return maxScore;
+}
+
+// Best Time to Buy and Sell Stock
+int maxProfit(vector<int> &prices)
+{
+    int n = prices.size();
+    int maxProfit = 0;
+    int minPrice = prices[0];
+
+    for (int i = 0; i < n; i++)
+    {
+        if (prices[i] < minPrice)
+        {
+            minPrice = prices[i];
+        }
+        else
+        {
+            maxProfit = max(maxProfit, prices[i] - minPrice);
+        }
+    }
+    return maxProfit;
+}
+
+// Rearrange Array Elements by Sign
+vector<int> rearrangeArray(vector<int> &nums)
+{
+    int n = nums.size();
+    vector<int> ans;
+    vector<int> positive;
+    vector<int> negative;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (nums[i] < 0)
+        {
+            negative.push_back(nums[i]);
+        }
+        else
+        {
+            positive.push_back(nums[i]);
+        }
+    }
+
+    for (int i = 0; i < positive.size(); i++)
+    {
+        ans.push_back(positive[i]);
+        ans.push_back(negative[i]);
+    }
+    return ans;
+}
+
+// Next Permutation
+void nextPermutation(vector<int> &nums)
+{
+    int n = nums.size();
+    int i = n - 2;
+
+    while (i >= 0 && nums[i] >= nums[i + 1])
+    {
+        i--;
+    }
+
+    if (i >= 0)
+    {
+        int j = n - 1;
+        while (j > i && nums[j] <= nums[i])
+        {
+            j--;
+        }
+        swap(nums[i], nums[j]);
+    }
+    reverse(nums.begin() + i + 1, nums.end());
+}
+
+// Array Leaders
+vector<int> leaders(vector<int> &arr)
+{
+    vector<int> ans;
+    int n = arr.size();
+    int maxRight = arr[n - 1];
+
+    for (int i = n - 1; i >= 0; i--)
+    {
+        if (arr[i] >= maxRight)
+        {
+            ans.push_back(arr[i]);
+            maxRight = arr[i];
+        }
+    }
+    reverse(ans.begin(), ans.end());
+    return ans;
+}
+
+// Longest Consecutive Sequence
+int longestConsecutive(vector<int> &nums)
+{
+    int n = nums.size();
+    unordered_set<int> st(nums.begin(), nums.end());
+    int longest = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (st.find(nums[i] - 1) == st.end())
+        {
+            int currSum = nums[i];
+            int currcount = 1;
+
+            while (st.find(currSum + 1) != st.end())
+            {
+                currSum++;
+                currcount++;
+            }
+            longest = max(longest, currcount);
+        }
+    }
+    return longest;
+}
+
+// Set Matrix Zeroes
+void markRow(vector<vector<int>> &matrix, int i, int m, int n)
+{
+    for (int j = 0; j < m; j++)
+    {
+        if (matrix[i][j] != 0)
+        {
+            matrix[i][j] = '*';
+        }
+    }
+}
+
+void markCol(vector<vector<int>> &matrix, int j, int m, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (matrix[i][j] != 0)
+        {
+            matrix[i][j] = '*';
+        }
+    }
+}
+
+void setZeroes(vector<vector<int>> &matrix)
+{
+    int n = matrix.size();
+    int m = matrix[0].size();
+
+    // First mark rows and columns that contain 0
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (matrix[i][j] == 0)
+            {
+                markRow(matrix, i, m, n);
+                markCol(matrix, j, m, n);
+            }
+        }
+    }
+
+    // second pass:  mark rows and columns that contain -1 to 1
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (matrix[i][j] == '*')
+            {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+}
+
+// Rotate Image
+void rotate(vector<vector<int>> &matrix)
+{
+    int n = matrix.size();
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j < n; j++)
+        {
+            swap(matrix[i][j], matrix[j][i]);
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        reverse(matrix[i].begin(), matrix[i].end());
+    }
+}
+
+// Spiral Matrix
+vector<int> spiralOrder(vector<vector<int>> &matrix)
+{
+    int n = matrix.size();
+    int m = matrix[0].size();
+    vector<int> ans;
+    int top = 0;
+    int left = 0;
+    int right = m - 1;
+    int bottom = n - 1;
+
+    while (top <= bottom && left <= right)
+    {
+        for (int i = left; i <= right; i++)
+        {
+            ans.push_back(matrix[top][i]);
+        }
+        top++;
+
+        for (int i = top; i <= bottom; i++)
+        {
+            ans.push_back(matrix[i][right]);
+        }
+        right--;
+
+        if (top <= bottom)
+        {
+            for (int i = right; i >= left; i--)
+            {
+                ans.push_back(matrix[bottom][i]);
+            }
+            bottom--;
+        }
+
+        if (left <= right)
+        {
+            for (int i = bottom; i >= top; i--)
+            {
+                ans.push_back(matrix[i][left]);
+            }
+            left++;
+        }
+    }
+    return ans;
+}
+
+// Subarray Sum Equals K
+int subarraySum(vector<int> &nums, int k)
+{
+    int n = nums.size();
+    int count = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        int sum = 0;
+        for (int j = i; j < n; j++)
+        {
+            sum += nums[j];
+            if (sum == k)
+            {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
 int main()
 {
     vector<int> arr;
@@ -308,7 +714,80 @@ int main()
     // cout << "The maximum number of consecutive ones in the array is: " << findMaxConsecutiveOnes(arr) << endl;
     // cout << "The single number in the array is: " << singleNumber(arr) << endl;
     // cout << "The length of the longest sub-array with sum K is: " << lenOfLongestSubarr(arr, 3) << endl;
-    cout << "The length of the longest sub-array with sum K is: " << lenOfLongestSubarr(arr, 15) << endl;
+    // cout << "The length of the longest sub-array with sum K is: " << lenOfLongestSubarr(arr, 15) << endl;
+    // vector<int> ans = twoSum(arr, 9);
+    // cout << "The indices of the two numbers that add up to the target sum are: ";
+    // for (int i = 0; i < ans.size(); i++)
+    // {
+    //     cout << ans[i] << " ";
+    // }
+
+    // sortColors(arr);
+    // cout << "The sorted array is: ";
+    // for (int i = 0; i < arr.size(); i++)
+    // {
+    //     cout << arr[i] << " ";
+    // }
+    // -2,1,-3,4,-1,2,1,-5,4]
+
+    // cout << "The majority element in the array is: " << majorityElement(arr) << endl;
+    // cout << "The maximum sum of the subarray is: " << maxSubArray(arr) << endl;
+    // cout << "The maximum profit from buying and selling stock is: " << maxProfit(arr) << endl;
+
+    // vector<int> ans = rearrangeArray(arr);
+    // cout << "The rearranged array is: ";
+    // for (int i = 0; i < ans.size(); i++)
+    // {
+    //     cout << ans[i] << " ";
+    // }
+
+    // nextPermutation(arr);
+    // cout << "The next permutation of the array is: ";
+    // for (int i = 0; i < arr.size(); i++)
+    // {
+    //     cout << arr[i] << " ";
+    // }
+
+    // vector<int> ans = leaders(arr);
+    // cout << "The leaders in the array are: ";
+    // for (int i = 0; i < ans.size(); i++)
+    // {
+    //     cout << ans[i] << " ";
+    // }
+
+    // cout << "The longest consecutive sequence in the array is: " << longestConsecutive(arr) << endl;
+
+    vector<vector<int>> matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+
+    // setZeroes(matrix);
+    // cout << "The matrix after setting zeroes is: ";
+    // for (int i = 0; i < matrix.size(); i++)
+    // {
+    //     for (int j = 0; j < matrix[0].size(); j++)
+    //     {
+    //         cout << matrix[i][j] << " ";
+    //     }
+    // }
+
+    // rotate(matrix);
+    // cout << "The matrix after rotating is: " << endl;
+    // for (int i = 0; i < matrix.size(); i++)
+    // {
+    //     for (int j = 0; j < matrix[0].size(); j++)
+    //     {
+    //         cout << matrix[i][j] << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // vector<int> ans = spiralOrder(matrix);
+    // cout << "The spiral order of the matrix is: ";
+    // for (int i = 0; i < ans.size(); i++)
+    // {
+    //     cout << ans[i] << " ";
+    // }
+
+    cout << "The number of subarrays with sum K is: " << subarraySum(arr, 3) << endl;
 
     return 0;
 }
